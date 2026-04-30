e0b8015f9a806f3e764f95b06b7224bd6f465980
wuzesh-art <wuzesh@gmail.com>
2026-04-30 23:46:40 +0800

    Update base URL in sitemap function

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = "https://mediabackup.io";
+  const baseUrl = "https://videodownloaderai.com";
   const routes = ["", "/privacy", "/terms", "/dmca", "/faq", "/platforms"];
 
   return routes.map((route) => ({
